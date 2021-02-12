580ac6d20169acac7eba15eafa016de31a798944
Stelios Kotanidis <stelioskotanidis@hotmail.com>
2021-02-12 22:06:05 +0000

    Toggle between preview img and content in interests section
    Make the interests section interactive and a bit more playful.
    Share the same placeholder img for now

--- src/components/Interests.tsx
+++ src/components/Interests.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
   Typography,
@@ -7,10 +7,13 @@ import {
   Card,
   CardHeader,
   CardContent,
+  CardMedia,
+  CardActionArea,
   GridList,
   GridListTile,
 } from "@material-ui/core";
 import { makeStyles, Theme } from "@material-ui/core/styles";
+import roads from "../images/roads.jpg";
 
 const useStyles = makeStyles((theme: Theme) => ({
   homeContainer: {
@@ -37,21 +40,31 @@ const useStyles = makeStyles((theme: Theme) => ({
 
 const Interests: React.FunctionComponent = () => {
   const classes = useStyles();
+  const [showTravel, setShowTravel] = useState(true);
+  const [showPhotography, setShowPhotography] = useState(true);
+  const [showReading, setShowReading] = useState(true);
+  const [showActivities, setShowActivities] = useState(true);
 
   const content = [
     {
       id: 1,
       title: "Traveling",
+      img: roads,
       description: `I grew up constantly moving between cities and
         countries and that's a good explanation of why I got the
         bug for traveling. I love exploring the unknown and making
         experiences while discovering different cultures and
         observing their lifestyle.
       `,
+      state: {
+        val: showTravel,
+        setter: setShowTravel,
+      },
     },
     {
       id: 2,
       title: "Photography",
+      img: roads,
       description: `Traveling and photography go hand in hand -
         it's a way of immortalising those memories and sharing
         them with the world. I've been experimenting with
@@ -60,10 +73,15 @@ const Interests: React.FunctionComponent = () => {
         it's the closest we can get to seeing the world through
         someone else's eyes.
       `,
+      state: {
+        val: showPhotography,
+        setter: setShowPhotography,
+      },
     },
     {
       id: 3,
       title: "Reading",
+      img: roads,
       description: `Ever since I was a child I loved reading
         fantasy books - I probably know more than I should about
         the Lord of the Rings saga. However, I've recently
@@ -71,10 +89,15 @@ const Interests: React.FunctionComponent = () => {
         literature to deeper ones like society or psychology
         that I love to have long conversations about.
       `,
+      state: {
+        val: showReading,
+        setter: setShowReading,
+      },
     },
     {
       id: 4,
       title: "Physical Activities",
+      img: roads,
       description: `As the Ancient Greeks said, a healthy
         mind exists in a healthy body. Exercising is a good way to
         unwind while also having fun playing a sport you enjoy and
@@ -84,6 +107,10 @@ const Interests: React.FunctionComponent = () => {
         there's always an idyllic walking route a stone's throw
         away!
       `,
+      state: {
+        val: showActivities,
+        setter: setShowActivities,
+      },
     },
   ];
 
@@ -98,10 +125,20 @@ const Interests: React.FunctionComponent = () => {
             {content.map((tile) => (
               <GridListTile key={tile.id}>
                 <Card elevation={0}>
-                  <Typography variant="h6" className={classes.homeContent}>
-                    <CardHeader title={tile.title} />
-                    <CardContent>{tile.description}</CardContent>
-                  </Typography>
+                  <CardActionArea
+                    onClick={() => tile.state.setter(!tile.state.val)}
+                  >
+                    <Typography variant="h6" className={classes.homeContent}>
+                      {tile.state.val ? (
+                        <CardMedia src={tile.img} component="img" />
+                      ) : (
+                        <>
+                          <CardHeader title={tile.title} />
+                          <CardContent>{tile.description}</CardContent>
+                        </>
+                      )}
+                    </Typography>
+                  </CardActionArea>
                 </Card>
               </GridListTile>
             ))}

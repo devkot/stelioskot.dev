3ec764cab052fe3b4e1421093674c22e52f30d9d
Stelios Kotanidis <stelioskotanidis@hotmail.com>
2021-02-20 15:28:34 +0000

    Extract navigation menu into its own component for modularity

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,82 +1,24 @@
 import React from "react";
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Button,
-  Theme,
-  Hidden,
-  IconButton,
-  MenuItem,
-  ClickAwayListener,
-  Grow,
-  MenuList,
-  Paper,
-  Popper,
-} from "@material-ui/core";
+import { AppBar, Toolbar, Typography, Button, Theme } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import MenuIcon from "@material-ui/icons/Menu";
+import NavigationMenu, { Sections } from "./NavigationMenu";
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
     flexGrow: 1,
   },
-  menuButtonBox: {
-    marginLeft: "auto",
-  },
 }));
-
-enum Sections {
-  Top = "#page-top",
-  About = "#about",
-  Projects = "#projects",
-  Interests = "#interests",
-  Contact = "#contact-me",
-}
-
-const MenuItemLink: React.FunctionComponent<{
-  section: string;
-  handleClose: any;
-  handleSectionClick: any;
-}> = ({ section, handleClose, handleSectionClick }) => {
-  return (
-    <MenuItem
-      onClick={handleSectionClick(Sections[section as keyof typeof Sections])}
-    >
-      {/* <SectionButton
-        section={section}
-        handleSectionClick={handleSectionClick}
-      /> */}
-      {section}
-    </MenuItem>
-  );
-};
-
-// const SectionButton: React.FunctionComponent<{
-//   section: string;
-//   handleSectionClick: any;
-// }> = ({ section, handleSectionClick }) => {
-//   return (
-//     <Button
-//       color="inherit"
-//       onClick={handleSectionClick(Sections[section as keyof typeof Sections])}
-//     >
-//       <Typography>{section}</Typography>
-//     </Button>
-//   );
-// };
 
 const Header: React.FunctionComponent = () => {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
 
   const handleSectionClick = (section: string) => (
-    event: React.MouseEvent<HTMLButtonElement>
+    event: React.MouseEvent<HTMLElement>
   ) => {
-    // setOpen(false);
     const anchor = (
       (event.target as HTMLDivElement).ownerDocument || document
     ).querySelector(section);
-    console.log(anchor);
 
     if (anchor) {
       setTimeout(
@@ -86,40 +28,6 @@ const Header: React.FunctionComponent = () => {
     }
     setOpen(false);
   };
-  const [open, setOpen] = React.useState(false);
-  const anchorRef = React.useRef<HTMLButtonElement>(null);
-
-  const handleToggle = () => {
-    setOpen((prevOpen) => !prevOpen);
-  };
-
-  const handleClose = (event: React.MouseEvent<EventTarget>) => {
-    if (
-      anchorRef.current &&
-      anchorRef.current.contains(event.target as HTMLElement)
-    ) {
-      return;
-    }
-
-    setOpen(false);
-  };
-
-  function handleListKeyDown(event: React.KeyboardEvent) {
-    if (event.key === "Tab") {
-      event.preventDefault();
-      setOpen(false);
-    }
-  }
-
-  // return focus to the button when we transitioned from !open -> open
-  const prevOpen = React.useRef(open);
-  React.useEffect(() => {
-    if (prevOpen.current === true && open === false) {
-      anchorRef.current!.focus();
-    }
-
-    prevOpen.current = open;
-  }, [open]);
 
   return (
     <AppBar position="sticky" color="secondary" className={classes.root}>
@@ -127,91 +35,11 @@ const Header: React.FunctionComponent = () => {
         <Button color="inherit" onClick={handleSectionClick(Sections.Top)}>
           <Typography>Stelios Kotanidis</Typography>
         </Button>
-        <Hidden smUp>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="end"
-            onClick={handleToggle}
-            ref={anchorRef}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Popper
-            open={open}
-            anchorEl={anchorRef.current}
-            role={undefined}
-            transition
-            disablePortal
-          >
-            {({ TransitionProps, placement }) => (
-              <Grow
-                {...TransitionProps}
-                style={{
-                  transformOrigin:
-                    placement === "bottom" ? "center top" : "center bottom",
-                }}
-              >
-                <Paper>
-                  <ClickAwayListener onClickAway={handleClose}>
-                    <MenuList
-                      autoFocusItem={open}
-                      id="menu-list-grow"
-                      onKeyDown={handleListKeyDown}
-                    >
-                      {Object.keys(Sections)
-                        .filter((section) => section !== "Top")
-                        .map((section) => (
-                          <MenuItemLink
-                            key={section}
-                            section={section}
-                            handleClose={handleClose}
-                            handleSectionClick={handleSectionClick}
-                          />
-                        ))}
-                    </MenuList>
-                  </ClickAwayListener>
-                </Paper>
-              </Grow>
-            )}
-          </Popper>
-          {/* <Menu
-            color="inherit"
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-            anchorEl={anchorEl}
-            PaperProps={{
-              style: {
-                maxHeight: ITEM_HEIGHT * 4.5,
-                width: "20ch",
-              },
-            }}
-          >
-            {Object.keys(Sections)
-              .filter((section) => section !== "Top")
-              .map((section) => (
-                <MenuItemLink
-                  key={section}
-                  section={section}
-                  handleClose={handleClose}
-                  handleSectionClick={handleSectionClick}
-                />
-              ))}
-          </Menu> */}
-        </Hidden>
-        {/* <Hidden xsDown>
-          <Box className={classes.menuButtonBox}>
-            {Object.keys(Sections)
-              .filter((section) => section !== "Top")
-              .map((section) => (
-                <SectionButton
-                  key={section}
-                  section={section}
-                  handleSectionClick={handleSectionClick}
-                />
-              ))}
-          </Box>
-        </Hidden> */}
+        <NavigationMenu
+          open={open}
+          setOpen={setOpen}
+          handleSectionClick={handleSectionClick}
+        />
       </Toolbar>
     </AppBar>
   );

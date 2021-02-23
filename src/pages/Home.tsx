e98848b7fb6364422448dc40a1cdd76e144d58ed
Stelios Kotanidis <stelioskotanidis@hotmail.com>
2021-02-23 21:52:42 +0000

    Only load png background if browser doesn't support it

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,21 +12,38 @@ import computepng from "../images/compute.png";
 const useStyles = makeStyles((theme: Theme) => ({
   background: {
     paddingTop: theme.spacing(10),
-    backgroundImage: `url(${computepng}), url(${compute})`,
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
     backgroundAttachment: "fixed",
+  },
+  backgroundWebpImage: {
+    backgroundImage: `url(${compute})`,
+  },
+  backgroundFallbackImage: {
+    backgroundImage: `url(${computepng})`,
   },
 }));
 
 const Home: React.FunctionComponent = () => {
   const classes = useStyles();
 
+  const supportsWebP = () => {
+    var elem = document.createElement("canvas");
+
+    return !!(elem.getContext && elem.getContext("2d"))
+      ? elem.toDataURL("image/webp").indexOf("data:image/webp") === 0
+      : false;
+  };
+
+  const backgroundImageStyle = supportsWebP()
+    ? classes.backgroundWebpImage
+    : classes.backgroundFallbackImage;
+
   return (
     <Container
       maxWidth={false}
       disableGutters={true}
-      className={classes.background}
+      className={`${classes.background} ${backgroundImageStyle}`}
     >
       <Intro />
       <About />
